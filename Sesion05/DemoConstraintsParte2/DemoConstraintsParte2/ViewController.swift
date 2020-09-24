1bce9a4ce1f0b73e2c30bf082cf4714ebbb01f8f
Kenyi Rodriguez <krodriguez@solera.pe>
2020-09-24 10:50:13 -0500

    Sesion05 parcial

---
+++ Sesion05/DemoConstraintsParte2/DemoConstraintsParte2/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DemoConstraintsParte2
+//
+//  Created by Kenyi Rodriguez on 9/24/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+         
+         let scrollView = UIScrollView()
+         scrollView.frame // te regresa la ubicación y el TAMAÑO FÍSICO del scroll o componente UI.
+         scrollView.contentSize // Te regresa el tamaño de todo el contenido tanto en vertical como en horizontal.
+         scrollView.contentOffset // Te regresa el desplazamiento del contenido.
+         
+         */
+        
+        /*
+         
+         alto = 400
+         altoContenido = 600
+         */
+    }
+}
+
